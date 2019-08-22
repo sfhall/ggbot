29decedea972bc09d7761612ad3c4c31158445f9
Sarah Hall <35583736+sfhall@users.noreply.github.com>
2019-08-22 16:30:13 -0400

    minor changes

--- gg.py
+++ gg.py
@@ -3,8 +3,8 @@ import markovify
 with open("nicemessages.txt", encoding="utf-8") as messages:
     text = messages.read()
 
-text_model = markovify.NewlineText(text)
+sentence = markovify.NewlineText(text, state_size=1)
 
 for i in range(1):
-    print (text_model.make_sentence(tries=50))
+    print (sentence.make_short_sentence(280))
 
